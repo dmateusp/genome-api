782d498f04481fbf872fbc7795d9f4017cc5472d
Daniel Pires <dmateusp@gmail.com>
2017-11-26 23:50:00 +0000

    test working

--- test/PersonSpec.hs
+++ test/PersonSpec.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 
 module PersonSpec where
 
@@ -8,22 +7,23 @@ import           Server                      (app)
 import           ServerState                 (ServerState (..), Environment (..),
                                               makePool, setLogger)
 import           Logger                      (defaultLogEnv)
-import           Safe                        (readMay)
 import           Test.Hspec
 import           Test.Hspec.Wai
 import           Test.Hspec.Wai.JSON
 import           Network.Wai                 (Application)
 import           Network.Wai.Handler.Warp    (run)
+import           Network.HTTP.Types.Header
 import           Api.Person
 import           Data.Aeson                  (encode)
-import           Data.ByteString.Lazy.Char8 as L (unpack)
-
 spec :: Spec
 spec = with appInstance $ do
   describe "/persons" $ do
       it "returns some persons" $ do
-        get "persons" `shouldRespondWith` [json|[]|] where
+        get "persons" `shouldRespondWith` personsResponse
 
+personsResponse =
+  let ResponseMatcher status headers body = [json|[]|]
+  in ResponseMatcher status [hContentType <:> "application/json;charset=utf-8"] body
 appInstance :: IO Application
 appInstance = do
          let port = 8083
