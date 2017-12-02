fa4ce787414eea9062018af06a6b12baeac7c7d6
Daniel Pires <dmateusp@gmail.com>
2017-12-02 22:42:27 +0000

    upsert working with relationship

--- src/Api/Person.hs
+++ src/Api/Person.hs
@@ -30,28 +30,34 @@ data Person =
   , role  :: Text
   , slack :: Text
   , email :: Text
+  , teamName :: Text  -- relationship
   } deriving (Eq, Show, Generic)
 
 instance ToJSON Person
 instance FromJSON Person
 instance ToTemplateParams Person where
-  toTemplateParams (Person name role slack email) = fromList $ [("name", T name), ("role", T role), ("slack", T slack), ("email", T email)]
-
+  toTemplateParams (Person name role slack email teamName) = fromList $
+    [("name", T name),
+    ("role", T role),
+    ("slack", T slack),
+    ("email", T email),
+    ("teamName", T teamName)]
 
 -- |Converts some BOLT value to Person
 toPerson :: Monad m => DB.Value -> m Person
 toPerson p = do node  :: Node <- exact p
                 let props = nodeProps node
-                name  :: Text <- (props `at` "name")  >>= exact
-                role  :: Text <- (props `at` "role")  >>= exact
-                slack :: Text <- (props `at` "slack") >>= exact
-                email :: Text <- (props `at` "email") >>= exact
-                return $ Person name role slack email
+                name     :: Text <- (props `at` "name")  >>= exact
+                role     :: Text <- (props `at` "role")  >>= exact
+                slack    :: Text <- (props `at` "slack") >>= exact
+                email    :: Text <- (props `at` "email") >>= exact
+                teamName :: Text <- (props `at` "teamName") >>= exact  -- that's wrong need to fix
+                return $ Person name role slack email teamName
 
 
 -- | Defining API and server
 type PersonApi =
-            "person" :> Capture "email" Text :> ReqBody '[JSON] Person :> Put '[JSON] NoContent
+            "person" :> Capture "email" Text :> ReqBody '[JSON] Person :> PutNoContent '[JSON] NoContent
 
 personServer :: MonadIO m => ServerT PersonApi (AppT m)
 personServer = upsertPerson
@@ -68,7 +74,8 @@ upsertPerson email' p = do
   return NoContent
   where
     cypher :: Text
-    cypher = "MERGE (p:Person {email: {emailUpdate}}) " <>
+    cypher = "MATCH (team:Team {name: {teamName}}) " <>
+             "MERGE (p: Person {email: {emailUpdate}})-[r:MEMBER_OF]->(team) " <>
              "SET p.name = {name}, " <>
                "p.role = {role}, "   <>
                "p.slack = {slack}, " <>
